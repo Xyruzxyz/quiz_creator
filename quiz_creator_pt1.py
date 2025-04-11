c0a93a6fa68dfbc9eedb704daa3727e3d46d667a
Xyruzxyz <rhexyruz@gmail.com>
2025-04-11 23:52:37 +0800

    Need to display all questions

--- quiz_creator_pt1.py
+++ quiz_creator_pt1.py
@@ -3,16 +3,16 @@
 #Ask another question until the user chose to exit.
 
 #Ask user for input
-input_question = input("Please enter your question: ")
-input_answer_a = input("Please enter answer a: ")
-input_answer_b = input("Please enter answer b: ")
-input_answer_c = input("Please enter answer c: ")
-input_answer_d = input("Please enter answer d: ")
-input_correct_answer = input("Please enter the correct answer (a, b, c or d): ")
-#display the question and answers to the user
-print("Question: " + input_question)
-print("a: " + input_answer_a)
-print("b: " + input_answer_b)
-print("c: " + input_answer_c)
-print("d: " + input_answer_d)
-print("Correct answer: " + input_correct_answer)
+#loop is needed to ask for multiple questions
+while True:
+    #ask user for question and answers
+    input_question = input("Please enter your question (or type 'exit' to quit): ")
+    if input_question.lower() == 'exit':
+        break
+    input_answer_a = input("Please enter answer a: ")
+    input_answer_b = input("Please enter answer b: ")
+    input_answer_c = input("Please enter answer c: ")
+    input_answer_d = input("Please enter answer d: ")
+    input_correct_answer = input("Please enter the correct answer (a, b, c or d): ")
+    
+
